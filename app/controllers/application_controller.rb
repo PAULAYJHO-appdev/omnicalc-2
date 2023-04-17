c34368401d7734132088e12a96de13ff6cddf610
Paula Ho <pho2@chicagobooth.edu>
2023-04-17 15:09:53 +0000

    Building Subtract Form and Calculation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,22 @@ class ApplicationController < ActionController::Base
     render({ :template => "calculation_templates/add_form.html.erb"})
   end
 
+  def add_calculation
+    @first_num = params.fetch("user_start").to_f
+    @second_num = params.fetch("user_end").to_f
+    @result = @first_num + @second_num
+    render({ :template => "calculation_templates/add_calculation.html.erb"})
+  end
+
+  def subtract_form
+    render({ :template => "calculation_templates/subtract_form.html.erb"})
+  end
+
+  def subtract_calculation
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @result = @second_num - @first_num
+    render({ :template => "calculation_templates/subtract_form.html.erb"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
 
   get("/add", { :controller => "application", :action => "add_form"})
   
+  get("/wizard_add", { :controller => "application", :action => "add_calculation"})
+
+  get("/subtract", { :controller => "application", :action => "subtract_form"})
+
+  get("/wizard_subtract", { :controller => "application", :action => "subtract_calculation"})
 end
